1f3448465aa43a1fcdff00730d1f54d30393fb4f
ardigiandi <ardiansyahg9080@gmail.com>
2024-03-09 00:46:26 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors : {
+        maroon : '#0C145A',
+        ungu : '#4D17E2',
+        card : '#F9FAFF',
+        birutua : '#1A1640',
+        button : '#4D17E2'
+      }
+    },
+  },
+  plugins: [],
+}
+
