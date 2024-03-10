8b26d4345cc7a0c2deb2fa0bb2e383aa8d35a81d
ardigiandi <ardiansyahg9080@gmail.com>
2024-03-11 00:50:46 +0700

    add : details page

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,8 @@ module.exports = {
         ungu : '#4D17E2',
         card : '#F9FAFF',
         birutua : '#1A1640',
-        button : '#4D17E2'
+        button : '#4D17E2', 
+        biru : '#CDF1FF'
       }
     },
   },
